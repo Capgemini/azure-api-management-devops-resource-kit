b21d9ae083d9bfa86a2187626a3fbe7c64fe0a45
Praveen Potturi <praveen.potturi@capgemini.com>
2022-03-25 12:20:09 +0530

    Custom Project

---
+++ src/ArmTemplates.Custom/Common/FileHandlers/MergeTemplatesFileNameGenerator.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common.FileHandlers;
+
+namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Custom.Common.FileHandlers
+{
+    public static class MergeTemplatesFileNameGenerator
+    {
+        public static string GenerateCreatorAPIFileName(string apiName, bool isSplitAPI, bool isInitialAPI)
+        {
+            // in case the api name has been appended with ;rev={revisionNumber}, take only the api name initially provided by the user in the creator config
+            string sanitizedAPIName = FileNameGenerator.GenerateOriginalAPIName(apiName);
+
+            if (isSplitAPI == true)
+            {
+                return isInitialAPI == true ? $@"/{sanitizedAPIName}-operations.json" : $@"/{sanitizedAPIName}-properties.json";
+            }
+            else
+            {
+                return $@"/{sanitizedAPIName}.json";
+            }
+        }
+    }
+}
