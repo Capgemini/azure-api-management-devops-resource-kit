c1312d8eae0519c3c8c54c4a50de7ce24e911f68
Praveen Potturi <praveen.potturi@capgemini.com>
2022-03-25 18:25:03 +0530

    Updates

---
+++ src/ArmTemplates.Custom/Common/Templates/Abstractions/TemplateParameterProperties.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common.Templates.Abstractions;
+
+namespace ArmTemplates.Custom.Common.Templates.Abstractions
+{
+    public class TemplateParameterPropertiesCustom : TemplateParameterProperties
+    {
+        public new string value { get; set; }
+    }
+}
---
+++ src/ArmTemplates.Custom/Creator/Models/TemplateParatrerConfig.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common.Templates.Abstractions;
+
+namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Custom.Creator.Models
+{
+    public class TemplateParatrerConfig: TemplateParameterProperties
+    {
+        public string name { get; set; }
+
+        public Dictionary<string,string> values { get; set; }
+    }
+}
